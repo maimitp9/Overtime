4ef39238add9fb162a9b76809793ebb86fac4fee
maimit <maimitp9@gmail.com>
2019-02-09 11:57:48 +0900

    Add AdminUser -> STI

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20190209023330) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.string "type"
