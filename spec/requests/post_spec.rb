e5bfc8aa28d2cedb4ee8c985f3a66d38a5b42732
Maimit <maimitp9@gmail.com>
2019-02-12 23:09:08 +0900

    remove feature_spec, create request_spec

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 describe "Post Page", type: :request do
-  let(:user) { create(:user) }
   describe "Unauthenticated user" do
     context "Index" do
       it "redirect to login page" do
@@ -49,6 +48,88 @@ describe "Post Page", type: :request do
       it "redirect to login page" do
         delete "/posts/#{post.id}"
         expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe "Authenticated User" do
+    let(:user) { create(:user) }
+    
+    before do
+      sign_in(user)
+    end
+
+    context "Index" do
+      let(:post) { build_stubbed(:post) }
+      it "redirect to post index" do
+        get "/posts"
+        expect(response).to render_template(:index)
+        expect(response.body).to include("Posts")
+      end
+    end
+
+    context "New" do
+      it "render new page" do
+        get "/posts/new"
+        expect(response).to render_template(:new)
+        expect(response).to_not render_template(:show)
+        expect(response.body).to include("New")
+      end
+    end
+
+    context "Create" do
+      let(:post_attributes) { attributes_for(:post) }
+      it "create post and redirect to show" do
+        post "/posts", params: { post: post_attributes }
+        
+        expect(response).to redirect_to(assigns(:post))
+        follow_redirect!
+
+        expect(response).to render_template(:show)
+        expect(response.body).to include("Post was created successfully")
+      end
+    end
+
+    context "Edit" do
+      let(:post) { create(:post) }
+      it "render edit page" do
+        get "/posts/#{post.id}/edit"
+        expect(response).to render_template(:edit)
+        expect(response.body).to include("Edit")
+        expect(response.body).to include(post.rational)
+      end
+    end
+
+    context "Update" do
+      let(:post) { create(:post) }
+      let(:post_attributes) { attributes_for(:post) }
+      it "update post and redirect to show page" do
+        put "/posts/#{post.id}", params: { post: post_attributes }
+        
+        expect(response).to redirect_to(assigns(:post))
+        follow_redirect!
+
+        expect(response).to render_template(:show)
+        expect(response.body).to include("Post was updated successfully")
+      end
+     end
+
+    context "Show" do
+      let(:post) { create(:post) }
+      it "redirect to show page" do
+        get "/posts/#{post.id}"
+        expect(response).to render_template(:show)
+        expect(response.body).to include(post.rational)
+      end
+    end
+
+    context "Destroy" do
+      let(:post) { create(:post) }
+      it "delete post and redirect to index" do
+        delete "/posts/#{post.id}"
+
+        expect(response).to redirect_to posts_path
+        expect(Post.find_by_id(post.id)).to eq(nil)
       end
     end
   end
