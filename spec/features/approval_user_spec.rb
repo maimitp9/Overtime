42bbd58ecb8067bbbf887bce31c96c90ad2db75a
Maimit <maimitp9@gmail.com>
2019-02-17 22:20:02 +0900

    only admin can edit the post status

--- spec/features/approval_user_spec.rb
+++ spec/features/approval_user_spec.rb
@@ -9,12 +9,25 @@ describe "navigate" do
 
   describe "edit post" do
     let(:post) { create(:post) }
-    it "has the status than can be editable on the form" do
-      visit edit_post_path(post)
+    context "admin user" do
+      it "has the status that can be editable on the form" do
+        visit edit_post_path(post)
 
-      choose("post_status_approved")
-      click_on("Save")
-      expect(post.reload.status).to eq("approved")
+        choose("post_status_approved")
+        click_on("Save")
+        expect(post.reload.status).to eq("approved")
+      end
+    end
+
+    context "regular user" do
+      let(:user) { create(:user) }
+      it "can't edit the status" do
+        logout(admin_user)
+        login_as(user)
+
+        visit edit_post_path(post)
+        expect(page).to_not have_content("Approved")
+      end
     end
   end
 
